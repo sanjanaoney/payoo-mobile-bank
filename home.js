81aae47a85572842328a7e68ee66fc0822376801
Sanjana <iamoney012@gmail.com>
2026-08-15 01:18:19 +0600

    cash-out added

--- home.js
+++ home.js
@@ -1,6 +1,6 @@
 const validPin=1234
 
-
+//add money feature
 // console.log("home js file connected")
 document.getElementById("add-money-btn")
 .addEventListener("click",function(e){
@@ -40,4 +40,31 @@ document.getElementById("available-balance").innerText=totalNewAvailableBalance
 
 
 
+})
+
+//cashout money feature
+
+document.getElementById("withdraw-btn").addEventListener("click",function(e){
+e.preventDefault()
+// console.log("withdraw button clicked")
+const amount=parseInt(document.getElementById("withdraw-amount").value)
+
+const availableBalance=parseInt(document.getElementById("available-balance").innerText)
+
+// console.log(amount,availableBalance)
+const totalNewAvailableBalance=availableBalance-amount
+console.log(totalNewAvailableBalance)
+document.getElementById("available-balance").innerText=totalNewAvailableBalance
+})
+
+
+// toggling feature
+
+document.getElementById("add-button").addEventListener("click",function(){
+    document.getElementById("cash-out-parent").style.display="none"
+    document.getElementById("add-money-parent").style.display="block"
+})
+document.getElementById("cash-out-button").addEventListener("click",function(){
+    document.getElementById("cash-out-parent").style.display="block"
+    document.getElementById("add-money-parent").style.display="none"
 })
